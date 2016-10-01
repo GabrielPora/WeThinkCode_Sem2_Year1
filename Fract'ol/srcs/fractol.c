910762040fbba583907b21f2f89f6764509d8fd1
Kalilo <kalilohansmann@gmail.com>
2016-10-01 09:12:18 +0200

    Added multiple window support.

--- Fract'ol/srcs/fractol.c
+++ Fract'ol/srcs/fractol.c
@@ -12,15 +12,36 @@
 
 #include "fractol.h"
 
+int		manage_args(t_env *env, int ac, char **av)
+{
+	pid_t	forked;
+
+	forked = 0;
+	if (ac < 2)
+		error_quit("Use: ./fractol [mandelbrot, julia, burningship]");
+	else if (ac == 2)
+		env->arg = av[1];
+	else
+	{
+		while (--ac > 1)
+		{
+			forked = fork();
+			if (forked)
+				break ;
+		}
+		env->arg = av[ac];
+	}
+	return ((int)forked);
+}
+
 int		main(int ac, char **av)
 {
 	t_env		*env;
 
-	if (ac != 2)
-		error_quit("Use: ./fractol [mandelbrot, julia, burningship]");
 	if (!(env = malloc(sizeof(*env))))
 		error_quit("Failed to malloc enviroment setup");
-	env_init(env, av[1]);
+	manage_args(env, ac, av);
+	env_init(env, env->arg);
 	mlx_key_hook(env->window->mlx_window, &key_hook, env);
 	mlx_expose_hook(env->window->mlx_window, &expose_hook, env);
 	mlx_hook(env->window->mlx_window, 6, (1 << 6), &mouse_hook, env);
