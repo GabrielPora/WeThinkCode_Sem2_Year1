94ccecc99b6266717af185d49d33671a49fd566f
GabrielPora <gabriel.groener@gmail.com>
2016-10-25 13:55:27 +0200

    made it understandable

--- mod1/srcs/event_listener.c
+++ mod1/srcs/event_listener.c
@@ -13,23 +13,23 @@ int		key_listener(int key, void *data)
 		temp = env->scenario;
 	printf("temp = %i\n", (int)temp);
 	printf("env->scenario = %i\n", (int)env->scenario);
-	if (key == 49)
+	if (key == 49) // spacebar
 	{
 		env->scenar_count += 50;
 		if (env->scenar_count > MAP_SIZE / 4. * 50.)
 			env->scenar_count = MAP_SIZE / 4. * 50.;
 	}
-	if (key == 35)
+	if (key == 35) // p
 		env->pause = !env->pause;
-	if (key == 3)
+	if (key == 3) // f
 		env->flow_pause = !env->flow_pause;
-	if (key == 53)
+	if (key == 53) // Esc
 		exit(0);
-	if (key == 14 && env->scenario == RAIN)
+	if (key == 14 && env->scenario == RAIN) // e
 		env->scenario = ESCAPING;
-	else if (key == 9 && env->scenario != ESCAPING)
+	else if (key == 9 && env->scenario != ESCAPING) // v
 		env->scenario = EMPTYING;
-	else if (key == 15)
+	else if (key == 15) // r
 	{
 		env->scenario = temp;
 		env->scenar_count = 0;
