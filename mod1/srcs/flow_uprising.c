95d9da3efedf0ea3be654e505857377b33fc999f
Kalilo <kalilohansmann@gmail.com>
2016-10-26 10:33:34 +0200

    Normed

--- mod1/srcs/flow_uprising.c
+++ mod1/srcs/flow_uprising.c
@@ -1,6 +1,5 @@
 
 #include "mod1.h"
-#define WTR(x, y) (env->water_tmp[x][y] + env->map[x][y])
 
 void	flow_uprising(t_env *env)
 {
@@ -14,10 +13,13 @@ void	flow_uprising(t_env *env)
 		y = 0;
 		while (y < MAP_SIZE)
 		{
-			if ((x > 0 && env->water_tmp[x - 1][y] > 0 && WTR(x - 1, y) > WTR(x, y))
-					|| (y > 0 && env->water_tmp[x][y - 1] > 0 && WTR(x, y - 1) > WTR(x, y))
-					|| (y < MAP_SIZE - 1 && env->water_tmp[x][y + 1] > 0 && WTR(x, y + 1) > WTR(x, y))
-					|| (x < MAP_SIZE - 1 && env->water_tmp[x + 1][y] > 0 && WTR(x + 1, y) > WTR(x, y)))
+			if ((x > 0 && E_WATER_T[x - 1][y] > 0 && WTR(x - 1, y) > WTR(x, y))
+					|| (y > 0 && E_WATER_T[x][y - 1] > 0 &&
+						WTR(x, y - 1) > WTR(x, y))
+					|| (y < MAP_SIZE - 1 && E_WATER_T[x][y + 1] > 0 &&
+						WTR(x, y + 1) > WTR(x, y))
+					|| (x < MAP_SIZE - 1 && E_WATER_T[x + 1][y] > 0 &&
+						WTR(x + 1, y) > WTR(x, y)))
 				env->water[x][y]++;
 			y++;
 		}
