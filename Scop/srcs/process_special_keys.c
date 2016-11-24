8faac9bb3efe8584ee34df8d8c01e9049beeb5ac
Kalilo <kalilohansmann@gmail.com>
2016-11-24 14:59:23 +0200

    Switched case statement for if statements.

--- Scop/srcs/process_special_keys.c
+++ Scop/srcs/process_special_keys.c
@@ -4,19 +4,10 @@ void process_special_keys(int key, int x, int y)
 {
 	(void)x;
 	(void)y;
-	switch(key)
-	{
-		case GLUT_KEY_F1 :
-				red = 1.0;
-				green = 0.0;
-				blue = 0.0; break;
-		case GLUT_KEY_F2 :
-				red = 0.0;
-				green = 1.0;
-				blue = 0.0; break;
-		case GLUT_KEY_F3 :
-				red = 0.0;
-				green = 0.0;
-				blue = 1.0; break;
-	}
+	if (key == GLUT_KEY_F1)
+		set_colour(1.0, 0.0, 0.0);
+	else if (key == GLUT_KEY_F2)
+		set_colour(0.0, 1.0, 0.0);
+	else if (key == GLUT_KEY_F3)
+		set_colour(0.0, 0.0, 1.0);
 }
