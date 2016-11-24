fcbd6792b74c39e59afb31f9b55d683c82e2c918
Kalilo <kalilohansmann@gmail.com>
2016-11-24 16:45:24 +0200

    Commented out mlx includes

--- Scop/includes/scop.h
+++ Scop/includes/scop.h
@@ -32,10 +32,10 @@
 # include <math.h>
 # include <libc.h>
 
-# include "../minilibx_macos/mlx.h"
-# include "../minilibx_macos/mlx_int.h"
-# include "../minilibx_macos/mlx_new_window.h"
-# include "../minilibx_macos/mlx_opengl.h"
+//# include "../minilibx_macos/mlx.h"
+//# include "../minilibx_macos/mlx_int.h"
+//# include "../minilibx_macos/mlx_new_window.h"
+//# include "../minilibx_macos/mlx_opengl.h"
 
 /*
 ** ----------\
