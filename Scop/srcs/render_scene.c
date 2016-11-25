2c79ab6a7919bd8bc1f838e2cd8359c58e24780b
Kalilo <kalilohansmann@gmail.com>
2016-11-25 07:45:56 +0200

    Added render vertex function.

--- Scop/srcs/render_scene.c
+++ Scop/srcs/render_scene.c
@@ -1,6 +1,27 @@
 #include "../includes/scop.h"
 
-void render_scene(void)
+void	render_vertex(void)
+{
+	t_list		*list;
+	t_vertex	*ver;
+	char		type;
+
+	list = lst;
+	glBegin(GL_POLYGON);
+	while (list)
+	{
+		ft_memcpy(&type, list->content, 1);
+		if (type == TYPE_VERTEX)
+		{
+			ver = (t_vertex *)list->content;
+			glVertex4f(ver->x, ver->y, ver->z, ver->w);
+		}		
+		list = list->next;
+	}
+	glEnd();
+}
+
+void	render_scene(void)
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glLoadIdentity();
