089bc301d7275ef87ac3d142e1f325fea8156f3f
Kalilo <kalilohansmann@gmail.com>
2016-11-25 09:50:16 +0200

    Normed

--- Scop/srcs/print_list.c
+++ Scop/srcs/print_list.c
@@ -6,17 +6,17 @@
 /*   By: khansman <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/25 09:37:45 by khansman          #+#    #+#             */
-/*   Updated: 2016/11/25 09:38:13 by khansman         ###   ########.fr       */
+/*   Updated: 2016/11/25 09:49:03 by khansman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/scop.h"
 
 /*
-**	This file is debug purposes, to test the content of the linked list
-*/
+ **	This file is debug purposes, to test the content of the linked list
+ */
 
-void	print_vetex(t_vertex *vertex)
+void		print_vetex(t_vertex *vertex)
 {
 	printf("\tTYPE:\tvetex\n");
 	printf("\tX:\t%f\n", vertex->x);
@@ -25,7 +25,7 @@ void	print_vetex(t_vertex *vertex)
 	printf("\tW:\t%f\n", vertex->w);
 }
 
-void	print_face(t_face *face)
+void		print_face(t_face *face)
 {
 	printf("\tTYPE:\tface\n");
 	printf("\tX:\t%f\n", face->x);
@@ -37,10 +37,29 @@ void	print_face(t_face *face)
 	printf("\tT_Z:\t%f\n", face->t_z);
 }
 
-void	print_list(t_list *list)
+static void	print_pos_details(t_list *pos)
+{
+	char	tmp;
+
+	printf("List Item: size: %zu; Has Content: %s, Has next: %s\n",
+			pos->content_size,
+			((pos->content != NULL) ? "YES" : "NO"),
+			((pos->next != NULL) ? "YES" : "NO"));
+	if (pos->content_size && pos->content != NULL)
+	{
+		ft_memcpy(&tmp, pos->content, 1);
+		if (tmp == TYPE_VERTEX)
+			print_vetex((t_vertex *)(pos->content));
+		else if (tmp == TYPE_FACE)
+			print_face((t_face *)(pos->content));
+		else
+			printf("\tTYPE:\tunknown\n");
+	}
+}
+
+void		print_list(t_list *list)
 {
 	t_list	*pos;
-	char	tmp;
 
 	if (list == NULL)
 	{
@@ -51,20 +70,7 @@ void	print_list(t_list *list)
 	ft_putendl("--- PRINTING LINKED LIST ---");
 	while (pos != NULL)
 	{
-		printf("List Item: size: %zu; Has Content: %s, Has next: %s\n",
-				pos->content_size,
-			((pos->content != NULL) ? "YES" : "NO"),
-			((pos->next != NULL) ? "YES" : "NO"));
-		if (pos->content_size && pos->content != NULL)
-		{
-			ft_memcpy(&tmp, pos->content, 1);
-			if (tmp == TYPE_VERTEX)
-				print_vetex((t_vertex *)(pos->content));
-			else if (tmp == TYPE_FACE)
-				print_face((t_face *)(pos->content));
-			else
-				printf("\tTYPE:\tunknown\n");
-		}
+		print_pos_details(pos);
 		pos = pos->next;
 	}
 	ft_putendl("--- DONE PRINTING LINKED LIST ---");
